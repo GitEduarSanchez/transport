6578f97765a2b8b20c17b0c2d526bc1b9256ec2f
samuel <samueljcp@gmail.com>
2024-11-07 16:29:40 -0500

    check

---
+++ Poliedro.Billing.Application/ControlViajeProducto/Commands/UpdatecontrolViajeProductoCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Poliedro.Billing.Domain.ControlViajeProducto.Request;
+
+namespace Poliedro.Billing.Application.ControlViajeProducto.Commands;
+
+public record UpdatecontrolViajeProductoCommand(int idControlViajeProducto, ControlViajeProductoRequest ControlViajeProductoEntity) : IRequest<Unit>;
+
---
+++ Poliedro.Billing.Domain/ControlViajeProducto/Request/controlViajeProductoRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Poliedro.Billing.Domain.ControlViajeProducto.Request;
+
+public record ControlViajeProductoRequest(int idControlViaje, int idProducto);
