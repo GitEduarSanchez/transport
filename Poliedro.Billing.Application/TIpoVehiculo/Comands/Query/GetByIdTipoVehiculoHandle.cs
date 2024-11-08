39a088d1f356e2bab4800cbf5009c2172bac1796
reyes <jundireyesb@gmail.com>
2024-11-08 00:04:46 -0500

    Fix

--- Poliedro.Billing.Application/TIpoVehiculo/Comands/Query/GetByIdTipoVehiculoHandle.cs
+++ Poliedro.Billing.Application/TIpoVehiculo/Comands/Query/GetByIdTipoVehiculoHandle.cs
@@ -1,14 +1,14 @@
-﻿using MediatR;
+using MediatR;
 using Poliedro.Billing.Application.TipoVehiculo.Dto;
 using Poliedro.Billing.Domain.TipoVehiculo.Ports;
 
 namespace Poliedro.Billing.Application.TipoVehiculo.Query;
 
-public class GetByIdTipoVehiculoHandle(ITipoVehiculoRepository tipovehiculoRepository) : IRequestHandler<GetByIdTipoVehiculoQuery, TipoVehiculoDto>
+public class GetByIdTipoVehiculoHandle(ITipoVehiculoRepository TipoVehiculoRepository) : IRequestHandler<GetByIdTipoVehiculoQuery, TipoVehiculoDto>
 {
     public async Task<TipoVehiculoDto> Handle(GetByIdTipoVehiculoQuery request, CancellationToken cancellationToken)
     {
-        var getByIdTipoVehiculo = await tipovehiculoRepository.GetById(request.Id);
-        return new TipoVehiculoDto(Id: getByIdTipoVehiculo.Id, Id: getByIdTipoVehiculo.Descripcion);
+        var getByIdTipoVehiculo = await TipoVehiculoRepository.GetById(request.Id);
+        return new TipoVehiculoDto(Id: getByIdTipoVehiculo.id, descripcion: getByIdTipoVehiculo.descripcion);
     }
 }
