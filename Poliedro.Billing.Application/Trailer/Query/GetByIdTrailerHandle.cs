761f7c0a1cc8b5f2035a03568f5faa16150ba538
Juan <kanekikun365@gmail.com>
2024-11-18 20:15:20 -0500

    actualizacion

---
+++ Poliedro.Billing.Application/Trailer/Query/GetByIdTrailerHandle.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Poliedro.Billing.Application.Estado.Commands.Dto;
+using Poliedro.Billing.Domain.Estado.Entities.Ports;
+
+namespace Poliedro.Billing.Application.Estado.Commands.Query;
+
+public class GetByIdEstadoHandle(IEstadoRepository estadoRepository) : IRequestHandler<GetByIdEstadoQuery, EstadoDto>
+{
+    public async Task<EstadoDto> Handle(GetByIdEstadoQuery request, CancellationToken cancellationToken)
+    {
+        var getByIdEstado = await estadoRepository.GetById(request.Id);
+        return new EstadoDto(Id: getByIdEstado.Id, Descripcion: getByIdEstado.Descripcion);
+    }
+}
