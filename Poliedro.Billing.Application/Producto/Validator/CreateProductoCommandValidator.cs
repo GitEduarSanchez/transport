3f45716e8614dd5103ba8ac964329b7aad33ab6f
greidin <greidinvp@gmail.com>
2024-12-05 21:12:13 -0500

    add 5 dic

--- Poliedro.Billing.Application/Producto/Validator/CreateProductoCommandValidator.cs
+++ Poliedro.Billing.Application/Producto/Validator/CreateProductoCommandValidator.cs
@@ -5,15 +5,14 @@ namespace Poliedro.Billing.ApplicationProducto.Commands.Validator;
 
 public class CreateProductoCommandValidator : AbstractValidator<CreateProductoCommand>
 {
-        public CreateProductoCommandValidator()
-         {
+    public CreateProductoCommandValidator()
+    {
         // Regla para la propiedad "descripcion": No puede estar vacía ni nula
         RuleFor(command => command.descripcion)
             .NotEmpty().WithMessage("La descripción es obligatoria.")
             .MaximumLength(255).WithMessage("La descripción no puede exceder los 255 caracteres.");
 
         // Regla para la propiedad "idunidad_medida": Debe ser mayor que 0
-        RuleFor(command => command.idunidad_medida)
-            .GreaterThan(0).WithMessage("El ID de la unidad de medida debe ser mayor que 0.");
+
     }
 }
